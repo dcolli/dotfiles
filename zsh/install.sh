4787174e47b146a089d0266e239cb1cc2eade204
dcolli1 <dan.collins@morningstar.com>
2016-12-23 22:48:31 -0600

    update zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,6 +14,13 @@ then
     then
         exit -1
     fi
+    
+    # zsh-autosuggestion
+    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+    # zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 fi
 
 exit 0
