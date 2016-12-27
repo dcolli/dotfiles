48c45c064d8fc6d0b46197aa3035db1b4aadae82
Dan Collins <daniel.patrick.collins@gmail.com>
2016-12-27 10:20:54 -0600

    😀

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -2,5 +2,10 @@
 #
 # Install iterm dynamic profile information
 # 
+
+if [[ ! -d  ~/Library/Application\ Support/iTerm2/DynamicProfiles ]] ; then
+    mkdir ~/Library/Application\ Support/iTerm2/DynamicProfiles/
+fi
+ 
 cp ./com.googlecode.iterm2.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 
--- zsh/install.sh
+++ zsh/install.sh
@@ -14,13 +14,12 @@ then
     then
         exit -1
     fi
-    
-    # zsh-autosuggestion
-    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
-    # zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 fi
+
+# zsh-autosuggestion
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 exit 0
