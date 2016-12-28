639794449896956be92436e7c98fbfd5139007e2
dan <daniel.patrick.collins@gmail.com>
2016-12-28 12:06:55 -0600

    😃

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -5,7 +5,8 @@
 CURRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ ! -d  ~/Library/Application\ Support/iTerm2/DynamicProfiles ]] ; then
-    mkdir ~/Library/Application\ Support/iTerm2/DynamicProfiles/
+    mkdir ~/Library/Application\ Support/iTerm2
+    mkdir ~/Library/Application\ Support/iTerm2/DynamicProfiles
 fi
 cp $CURRDIR/com.googlecode.iterm2.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 
